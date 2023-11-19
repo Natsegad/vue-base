26aa34f22afa837cb425284b34038ae68a7e5313
Natsedag <xbydji@gmail.com>
2023-11-19 20:57:12 +0300

    feat: load project

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header">
+    <div class="header__left">
+        <a href="/catalog" class="header__text text-small">Каталог</a>
+        <a class="header__text text-small">Каталог</a>
+        <a class="header__text text-small">Каталог</a>
+        <a class="header__text text-small">Каталог</a>
+    </div>
+    <div class="header__middle"></div>
+    <div class="header__right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+.header {
+  background: var(--main-color);
+  &__text{
+
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home-page">
+    <div class="home-page__header">
+      <Header />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Header from "../components/BaseHeader.vue";
+</script>
+
+<style lang="scss" scoped>
+p {
+  font-size: var(--header-font-size);
+}
+</style>
